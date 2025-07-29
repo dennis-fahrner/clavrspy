812c5d6268d5c9eb5f02303bd1e84c8fbc9c0277
fade1011 <fade1011@h-ka.de>
2025-07-29 23:21:23 +0200

    updated Naming scheme for TestRunner

--- test/run_tests.py
+++ test/run_tests.py
@@ -7,11 +7,10 @@ if __name__ == '__main__':
     Config.PRINT_LIVE = True
     Config.SINGLE_LINE_STACK = True
 
-    print(f"<Using clavrs{get_clavrs_version()}.exe>")
     loader = unittest.TestLoader()
     suite = loader.loadTestsFromName('test.test_cases')
 
-    runner = TestRunner(verbosity=0)
+    runner = TestRunner(verbosity=0, title=f"Unit Test Report <{get_clavrs_version()}>")
     result = runner.run(suite)
 
     sys.exit(0 if result.wasSuccessful() else 1)
